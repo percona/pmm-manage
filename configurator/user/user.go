da75fd728c53422ca1d267054a98c6ec7837fdc8
Mykola Marzhan <delgod@delgod.com>
2017-03-08 14:16:32 +0200

    improve error messages

--- configurator/user/user.go
+++ configurator/user/user.go
@@ -11,23 +11,23 @@ var PMMConfig config.PMMConfig
 // CreateUser in .htpasswd file, Prometheus config and Grafana database
 func CreateUser(newUser PMMUser) (string, error) {
 	if strings.ContainsAny(newUser.Username, ":#") || len(newUser.Username) == 0 || len(newUser.Username) > 255 {
-		return "Usernames are limited to 255 bytes and may not include colon and hash symbols", nil
+		return "User name is limited to 255 bytes and may not include colon and hash symbols", nil
 	}
 
 	if len(newUser.Password) == 0 || len(newUser.Password) > 255 {
-		return "Passwords are limited to 255 bytes", nil
+		return "Password is limited to 255 bytes", nil
 	}
 
 	if err := createGrafanaUser(newUser); err != nil {
-		return "Cannot set grafana password", err
+		return "Cannot set Grafana password", err
 	}
 
 	if err := replacePrometheusUser(newUser); err != nil {
-		return "Cannot set prometheus password", err
+		return "Cannot set Prometheus password", err
 	}
 
 	if err := createHTTPUser(newUser); err != nil {
-		return "Cannot set http password", err
+		return "Cannot set HTTP password", err
 	}
 
 	return "success", nil
@@ -37,11 +37,11 @@ func CreateUser(newUser PMMUser) (string, error) {
 // TODO: check user in Prometheus and replace to default if needed
 func DeleteUser(username string) (string, error) {
 	if err := deleteGrafanaUser(username); err != nil {
-		return "Cannot remove grafana user", err
+		return "Cannot remove Grafana user", err
 	}
 
 	if err := deleteHTTPUser(username); err != nil {
-		return "Cannot remove http user", err
+		return "Cannot remove HTTP user", err
 	}
 
 	return "success", nil
